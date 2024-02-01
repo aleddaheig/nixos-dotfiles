b8da48ce6dad0c86bb56109a70b1cdda5875da9b
Anthony Ledda <anthony.ledda@heig-vd.ch>
2024-02-01 13:29:24 +0100

    single sign on with luks

--- fw-al.nix
+++ fw-al.nix
@@ -5,6 +5,10 @@
 
   networking.useDHCP = lib.mkDefault true;
 
+  # Start systemd on early stage
+  boot.initrd.systemd.enable = true;
+  boot.initrd.systemd.enableTpm2 = false;
+
   boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ "dm-snapshot" "tpm_crb" ];
   boot.kernelModules = [ "kvm-amd" ];
@@ -16,19 +20,19 @@
       preLVM = true;
   };
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/7bc9f6d1-0a7d-4d44-82e6-b7e5b4ccf83d";
+  fileSystems."/" = {
+      device = "/dev/disk/by-uuid/7bc9f6d1-0a7d-4d44-82e6-b7e5b4ccf83d";
       fsType = "ext4";
-    };
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/5981-0995";
+  fileSystems."/boot" = {
+      device = "/dev/disk/by-uuid/5981-0995";
       fsType = "vfat";
-    };
+  };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/15ad9535-b26a-485e-9ca9-943d773ebbc5"; }
-    ];
+  swapDevices = [
+    { device = "/dev/disk/by-uuid/15ad9535-b26a-485e-9ca9-943d773ebbc5"; }
+  ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
@@ -44,6 +48,15 @@
     '';
     extraGSettingsOverridePackages = [ pkgs.gnome.mutter ];
   };
+
+  services.xserver.displayManager.autoLogin = {
+    enable = true;
+    user = "tony";
+  };
+
+  # Workaround auto-login gnome-shell crash
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.tony = {
