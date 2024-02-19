3e4ac8c941b81a4acb1e0048e24c1f2ec080c515
Anthony Ledda <anthony.ledda@heig-vd.ch>
2024-02-19 16:44:51 +0100

    virtualbox

--- system.nix
+++ system.nix
@@ -90,5 +90,9 @@
   nix.settings.substituters = [ "https://nix-community.cachix.org" "https://cache.nixos.org/" ];
   nix.settings.trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
 
+  virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
+  users.extraGroups.vboxusers.members = [ "tony" ];
+
   system.stateVersion = "23.11";
 }
