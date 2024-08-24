ab4d7a256e401823b94f7bd068c93b527ca82145
Anthony Ledda <anthony.ledda@heig-vd.ch>
2024-08-24 22:12:48 +0200

    better fingerprint and get rid of autologin

--- fw-al.nix
+++ fw-al.nix
@@ -49,14 +49,29 @@
     extraGSettingsOverridePackages = [ pkgs.gnome.mutter ];
   };
 
-  services.displayManager.autoLogin = {
-    enable = true;
-    user = "tony";
-  };
+  services.fprintd.enable = true;
 
-  # Workaround auto-login gnome-shell crash
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
+  security.pam.services.login.fprintAuth = false;
+  # similarly to how other distributions handle the fingerprinting login
+  security.pam.services.gdm-fingerprint = lib.mkIf (config.services.fprintd.enable) {
+        text = ''
+          auth       required                    pam_shells.so
+          auth       requisite                   pam_nologin.so
+          auth       requisite                   pam_faillock.so      preauth
+          auth       required                    ${pkgs.fprintd}/lib/security/pam_fprintd.so
+          auth       optional                    pam_permit.so
+          auth       required                    pam_env.so
+          auth       [success=ok default=1]      ${pkgs.gnome.gdm}/lib/security/pam_gdm.so
+          auth       optional                    ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so
+
+          account    include                     login
+
+          password   required                    pam_deny.so
+
+          session    include                     login
+          session    optional                    ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so auto_start
+    '';
+  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.tony = {
