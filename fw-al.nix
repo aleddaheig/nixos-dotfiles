3b5bd578690aaca5e6bda63887f51cc6f8645732
Anthony Ledda <anthony.ledda@heig-vd.ch>
2024-10-19 12:42:28 +0200

    Edit networking + add support for LocalSend

--- fw-al.nix
+++ fw-al.nix
@@ -1,9 +1,20 @@
 # fw-al.nix
 { config, lib, pkgs, ... }: {
 
-  networking.hostName = "fw-al";
-
-  networking.useDHCP = lib.mkDefault true;
+  # Networking
+  networking = {
+    hostName = "fw-al";
+    useDHCP = lib.mkDefault true;
+    firewall = {
+       enable = true;
+       allowedTCPPorts = [ 80 443 53317 ]; # 53317 is for LocalSend
+       allowedUDPPortRanges = [
+         { from = 4000; to = 4007; }
+         { from = 53315; to = 53318; }
+         { from = 8000; to = 8010; }
+       ];
+     };
+  };
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
