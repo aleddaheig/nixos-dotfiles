7f7e49ebcf78098e947c3b72af9a2ef88a92bc75
Anthony Ledda <anthony.ledda@heig-vd.ch>
2024-11-11 14:25:52 +0100

    specialPkgs

--- home/modules/logseq.nix
+++ home/modules/logseq.nix
@@ -1,9 +1,9 @@
 { lib, pkgs, ... }:
-
 let
+  specialPkgs = import ../../.private/specialPkgs.nix { inherit pkgs; };
   logseq = pkgs.logseq.overrideAttrs (oldAttrs: {
     postFixup = ''
-              makeWrapper ${pkgs.electron_31}/bin/electron $out/bin/${oldAttrs.pname} \
+              makeWrapper ${specialPkgs.electronLogseq}/bin/electron $out/bin/${oldAttrs.pname} \
       	  --set "LOCAL_GIT_DIRECTORY" ${pkgs.git} \
       	  --add-flags $out/share/${oldAttrs.pname}/resources/app \
       	  --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}" \
